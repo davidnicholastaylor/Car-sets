ee167fcb58bd2a5df5c13227b7b0d05fa21783ac
davidnicholastaylor <dntaylor14@gmail.com>
2018-10-05 14:39:43 -0500

    completed first half of instructions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace sets
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace sets {
+    class Program {
+        static void Main (string[] args) {
+
+            HashSet<string> ShowRoom = new HashSet<string> ();
+
+            ShowRoom.Add("Taurus");
+            ShowRoom.Add("Camry");
+            ShowRoom.Add("Accord");
+            ShowRoom.Add("Focus");
+
+            ShowRoom.Add("Taurus");
+
+            HashSet<string> UsedLot = new HashSet<string> ();
+
+            UsedLot.Add("Corolla");
+            UsedLot.Add("Mustang");
+
+            ShowRoom.UnionWith(UsedLot);
+
+            ShowRoom.Remove("Taurus");
+
+            foreach (string vehicle in ShowRoom) {
+                Console.WriteLine ($"{vehicle}");
+            }
         }
     }
 }
